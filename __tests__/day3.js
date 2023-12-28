161be4ffa02a5096a5efd5162fd0d0444d1c17fc
Sergio Giraldo <sergio@giraldo.com.br>
2023-12-28 21:31:24 +0100

    feat!: day three completed ::by sergio giraldo @ 20231228T2131CET, gpg signed

--- __tests__/day3.js
+++ __tests__/day3.js
@@ -2,22 +2,26 @@ const lib = require("../solutions/lib/day3");
 
 test("SolveFirstStar", () => {
 	let lines = [];
+
 	lines.push("vJrwpWtwJgWrhcsFMMfFFhFp");
 	lines.push("jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL");
 	lines.push("PmmdzqPrVvPwwTWBwg");
 	lines.push("wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn");
 	lines.push("ttgJtRGJQctTZtZT");
 	lines.push("CrZsJsPPZsGzwwsLwLmpwMDw");
+
 	expect(lib.solveForFirstStar(lines)).toBe(157);
 });
 
 test("SolveSecondStar", () => {
 	let lines = [];
+
 	lines.push("vJrwpWtwJgWrhcsFMMfFFhFp");
 	lines.push("jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL");
 	lines.push("PmmdzqPrVvPwwTWBwg");
 	lines.push("wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn");
 	lines.push("ttgJtRGJQctTZtZT");
 	lines.push("CrZsJsPPZsGzwwsLwLmpwMDw");
+	
 	expect(lib.solveForSecondStar(lines)).toBe(70);
 });
