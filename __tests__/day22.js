8c4533ee38165d249b835b95c8c7263475eaff56
Sergio Giraldo <sergio@giraldo.com.br>
2024-01-06 17:11:00 +0100

    feat!: day 22 completed ::by sergio giraldo @ 20240106T1711CET, gpg signed

---
+++ __tests__/day22.js
@@ -0,0 +1,44 @@
+const lib = require("../solutions/lib/day22");
+const helpers = require("../solutions/lib/helpers.js");
+
+test("SolveFirstStar", () => {
+	helpers.which.env = "test";
+
+	let lines = [];
+	lines.push("        ...# ");
+	lines.push("        .#.. ");
+	lines.push("        #... ");
+	lines.push("        .... ");
+	lines.push("...#.......# ");
+	lines.push("........#... ");
+	lines.push("..#....#.... ");
+	lines.push("..........#. ");
+	lines.push("        ...#....");
+	lines.push("        .....#..");
+	lines.push("        .#......");
+	lines.push("        ......#.");
+	lines.push("");
+	lines.push("10R5L5R10L4R5L5");
+	expect(lib.solveForFirstStar(lines)).toBe(6_032);
+});
+
+test("SolveSecondStar", () => {
+	helpers.which.env = "test";
+
+	let lines = [];
+	lines.push("        ...# ");
+	lines.push("        .#.. ");
+	lines.push("        #... ");
+	lines.push("        .... ");
+	lines.push("...#.......# ");
+	lines.push("........#... ");
+	lines.push("..#....#.... ");
+	lines.push("..........#. ");
+	lines.push("        ...#....");
+	lines.push("        .....#..");
+	lines.push("        .#......");
+	lines.push("        ......#.");
+	lines.push("");
+	lines.push("10R5L5R10L4R5L5");
+	expect(lib.solveForSecondStar(lines, 4)).toBe(5_038);
+});
